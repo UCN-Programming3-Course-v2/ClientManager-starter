a77e9740acd4ddc811ff5ef82ea3964014c741fb
Lars Nysom <LANY@ucn.dk>
2021-09-21 09:23:45 +0200

    Added TODO with compile error

--- ClientManager.DataAccessLayer/CustomerDao.cs
+++ ClientManager.DataAccessLayer/CustomerDao.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace ClientManager.DataAccessLayer
 {
+    // TODO: Solve the compile error
     class CustomerDao : Dao, ICustomerDao
     {
         public IEnumerable<Customer> GetAll()
